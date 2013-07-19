04dfa6472f7b68c93c62f58fc02d61e95ea2edf0
gutsnosada <gutsnosada@yahoo.co.jp>
2013-07-19 20:14:55 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_secret: ''
+  consumer_key: 'tT7idplJwEZso67ocVO0w',
+  consumer_secret: 'sRmILsqMtZmsMuna6S8XNNu6Y5JzTVVYDR8i4P4',
+  access_token: '1262405046-OFGW5zpF31mzfcnmMjn9sjwkvJfeldyiF9H38ak',
+  access_secret: '655rrrn9D03InmQieOSpFYTI8gIMC4Cg7FRjE7BI'
 };
